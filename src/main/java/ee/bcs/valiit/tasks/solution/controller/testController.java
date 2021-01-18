427ab790074a1cb42b525cba6b04b640e97a5869
Kristian Semjonov <kristian.semjonov@gmail.com>
2021-01-18 14:15:00 +0200

    18.01

---
+++ src/main/java/ee/bcs/valiit/tasks/solution/controller/testController.java
@@ -0,0 +1,16 @@
+package ee.bcs.valiit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("test/{name}")
+    public String getHelloWorld(@PathVariable("name") String userName, @RequestParam("name") String lastName) {
+        return "Hello World " + userName + lastName;
+    }
+
+}
